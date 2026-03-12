d3bbaf846b4a75f3b30b3cce6d5b929e4ba1d93c
PadarbindaPuhan <padarbinda.puhan@gmail.com>
2026-03-12 09:16:19 +0530

    feat: call ami bulder

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,5 +10,15 @@ module "ami_fetcher" {
   ami_os_type            = var.ami_os_type
   baking_recipe_playbook = var.baking_recipe_playbook
   application_name       = var.application_name
+}
+
+module "ami_builder" {
+  source = "github.com/BakeFoundry/bk-bake-ami-module?ref=e886d3c6f419f78aa4d0419639cf920e6c9a10d5"
+
+  depends_on = [module.ami_fetcher]
+
+  source_ami_id          = module.ami_fetcher.ami_id
+  baking_recipe_playbook = var.baking_recipe_playbook
+  application_name       = var.application_name
   version_tag            = var.version_tag
 }
