451cda96d0e0b6a49e35b84ff2ada55728dabcd3
Dilip <dilip.samanta@gmail.com>
2026-03-12 20:44:00 +0530

    fix: remove ami_builder module from terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,3 @@ module "bake-ami" {
   baking_recipe_playbook = var.baking_recipe_playbook
   application_name       = var.application_name
 }
-
-module "ami_builder" {
-  source = "github.com/BakeFoundry/bk-bake-ami-module?ref=f1050330dbffbf9e1cdc058b48a70bfab13ff993"
-
-  depends_on = [module.ami_fetcher]
-
-  source_ami_id          = module.ami_fetcher.ami_id
-  baking_recipe_playbook = var.baking_recipe_playbook
-  application_name       = var.application_name
-  version_tag            = var.version_tag
-}
